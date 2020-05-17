1fb9ba88192b0583490378220bf947268a4a6b27
Ayush Sharma <ayushs217@gmail.com>
2020-05-17 13:47:21 -0400

    Updates

---
+++ precache-manifest.a3e377c1334a69fa5ae2cab35b061fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b5dbed26e7ad3871b804eb5fd38d8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18762c53b296d9f4b0ed",
+    "url": "/static/css/main.a9327539.chunk.css"
+  },
+  {
+    "revision": "56e20ee7bd963655df07",
+    "url": "/static/js/2.73a6d7d1.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.73a6d7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18762c53b296d9f4b0ed",
+    "url": "/static/js/main.9af1dbae.chunk.js"
+  },
+  {
+    "revision": "d4da25060a36d2680181",
+    "url": "/static/js/runtime-main.e9b015e2.js"
+  }
+]);
